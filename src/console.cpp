bf558561f4567d1a5089718e67c34286cd92ea03
Akoro <greengryphus@gmail.com>
2019-12-10 15:58:32 +0300

    V0.0

---
+++ src/console.cpp
@@ -0,0 +1,67 @@
+#include <Arduino.h>
+#include "console.h"
+
+// 2019-12-06
+
+void Console::run(void)
+{
+  if(dev->available())
+  {
+    char c = dev->read();
+    if(c>=' ' && c<127) // ввод команды
+    {
+      line += c;
+      dev->write(c);
+    }
+    else if(c == '\r')  // команда принята,
+    {
+      dev->write("\r\n");
+      line.trim();
+      if(!line.isEmpty()) 
+        parse();        // разбор команды
+      line.clear();
+      dev->write(prompt.c_str());
+    }
+    else if(!line.isEmpty() && (c == '\b' || c == 127)) // забой
+    {
+      line.remove(line.length()-1);
+      dev->write("\b \b");
+    }
+  }
+}
+
+void Console::parse(void)
+{
+  int i = line.indexOf(' ');
+  String cmd = (i != -1) ? line.substring(0,i) : line;
+  cmd.trim();
+  if(handlers.find(cmd) != handlers.end())
+  {
+    String args = line.substring(line.indexOf(' '));
+    args.trim();
+    ArgList pl(&args);
+    (*handlers[cmd])(pl, *dev);
+  }
+  else
+    if(unknown != nullptr)
+      (*unknown)(line, *dev);
+    else
+      dev->println("?");
+}
+
+String ArgList::getNextArg(char del)
+{
+  String p = "";
+  if(idx < 0) 
+    return p;
+  int j = args->indexOf(del, idx);
+  if(j > -1)
+  {
+    p = args->substring(idx,j); 
+    while((*args)[++j] == ' ');
+  }
+  else
+    p = args->substring(idx); 
+  idx = j;
+  return p;
+}
---
+++ src/console.h
@@ -0,0 +1,43 @@
+// 2019-12-06
+
+#pragma once
+
+#include <map>
+
+class ArgList
+{
+  public:
+  ArgList(String* p){args = p; idx = 0;}
+  String getNextArg(char del = ' ');
+  private:
+  String* args;
+  int idx;
+};
+
+using Handler  = void(*)(ArgList&, Stream&);
+using HUnknown = void(*)(String&, Stream&);
+
+class Console
+{
+  public:
+  Console(Stream* s) {line.clear(); handlers.clear(); dev = s; defPrompt(); unknown = nullptr;}
+  Console(void): Console(&Serial){}
+  void setPrompt(const String p){prompt = p;}
+  void defPrompt(void){prompt = ">";}
+  void start(void){line.clear(); dev->println(); dev->print(prompt);}
+  void run(void);
+  bool busy(void) const {return !line.isEmpty();}
+  Stream& stream(void) const {return *dev;};
+  Stream& operator()(void) const {return stream();}
+  void onCmd(const String& t, const Handler h){if(h != nullptr && !t.isEmpty()) handlers[t] = h;} // set handler for the command
+  void onUnknown(HUnknown u) {unknown=u;}
+  
+  private:
+  std::map<String,Handler> handlers; // dictionary command:handler
+  Stream *dev;      // in/out device (stream)
+  String line;      // command line: <command>[ <par1>[ <par2>[ ...]]]
+  String prompt;    // current prompt
+  HUnknown unknown; // handler for unrecognized commands
+  void parse(void); // commands parsing
+};
+
